166c58178402f5cf9ee96bce2dc220c7a48a62ad
Robert Forkel <xrotwang@googlemail.com>
2022-07-20 07:54:35 +0200

    upgrade clld

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld>=8',  # >=7.0
+        'clld>=9.2.1',
         'clldmpg>=4.2',
         'pyclts',
         'pyglottolog',
